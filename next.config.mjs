a4abb3b92172caa90fe09f5f855629ee6e669277
manjuhiremath <manjuhiremath1352@gmail.com>
2025-11-05 15:53:20 +0530

    prod fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  // reactCompiler: true,
-  serverExternalPackages: ["pdf-parse"],
     experimental: {
     optimizeCss: false, // ← Disable lightningcss
 
   },
-   webpack: (config, { isServer }) => {
+  webpack: (config) => {
+    config.cache = false;
     return config;
   },
 };
